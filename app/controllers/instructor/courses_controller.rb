308dab4cc66e72fabd94d00fc760f74a8aeec48b
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-09-11 21:46:36 +0000

    course and lesson permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -15,10 +15,20 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render :text => "Unauthorized", :status => :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,16 +1,22 @@
 class Instructor::SectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
+
   def new
-    @course = Course.find(params[:course_id])
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
     @section = @course.sections.create(section_params)
     redirect_to instructor_course_path(@course)
   end
 
   private
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
 
   def section_params
     params.require(:section).permit(:title)
