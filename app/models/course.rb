6e26572cb12b74bf425a6ada80c56ed89e6f2cb9
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-09-21 14:53:01 +0000

    add carrierwave/figaro/AWS config

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,12 @@
 class Course < ActiveRecord::Base
   belongs_to :user
   has_many :sections
+  has_many :images
 
   validates :title, :presence => true
   validates :description, :presence => true
   validates :cost, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
+
+  mount_uploader :image, ImageUploader
+
 end
