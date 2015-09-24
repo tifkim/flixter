cd5aa68ed2bb09c25a475ae97612d39f0479d077
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-09-24 00:42:34 +0000

    add stripe gem

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :enrolled_user
+
   def show
   end
 
   private
 
   helper_method :current_lesson
+
+  def enrolled_user
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => 'You must be enrolled to view lessons in detail.'
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
