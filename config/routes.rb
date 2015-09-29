f0d7f74bce49e8ade6d7ebd2bab52fcbe297b06e
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-09-29 19:33:06 +0000

    format splash page, add feature descriptions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Flixter::Application.routes.draw do
   resource :dashboard, :only => [:show]
   root 'static_pages#index'
   get 'privacy', :to => 'static_pages#privacy'
+  get 'team', :to => 'static_pages#team'
+  get 'careers', :to => 'static_pages#careers'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
   end
