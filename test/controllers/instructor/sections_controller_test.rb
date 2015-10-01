1f706b4b7bc5af2bf9bf14259ed43d010c80c33e
Tiffany Kim <kim.tiffany.min@gmail.com>
2015-10-01 00:47:28 +0000

    added problematic course controller

--- test/controllers/instructor/sections_controller_test.rb
+++ test/controllers/instructor/sections_controller_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class Instructor::SectionsControllerTest < ActionController::TestCase
 
-  # test "section created" do
-  #   user = FactoryGirl.create(:user)
-  #   sign_in user
-  #   course = FactoryGirl.create(:course)
-  #
-  #   assert_difference('Section.count') do
-  #     post :create, {:section => {:title => 'Test'}}
-  #   end
-  # end
+  test "section created" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    course = FactoryGirl.create(:course)
 
-# assert_redirected_to instructor_course_path(Course.last)
+    assert_difference('Section.count') do
+      post :create, :course_id => course.id, :section => {:title => 'Test'}
+    end
+
+    assert_redirected_to instructor_course_path(Course.last)
+  end
+
 end
